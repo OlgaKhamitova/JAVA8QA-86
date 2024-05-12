1ea2fb81ef45506851ceb9de1f31975e8dcec58a
Olga Khamitova <olgacherkasova13317@rambler.ru>
2024-05-12 17:48:13 +0300

    New test added

--- src/main/java/org/example/java8qa86/Main.java
+++ src/main/java/org/example/java8qa86/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.java8qa86;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/test/java/StatisticsServiceTest.java
@@ -0,0 +1,29 @@
+package ru.netology.statistic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.example.java8qa86.StatisticsService;
+
+public class StatisticsServiceTest {
+
+    @Test
+    public void findMax() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void findMaxIfIncome() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 15};
+        long expected = 15;
+        long actual = service.findMax(incomesInBillions);
+        Assertions.assertEquals(expected, actual);
+    }
+}
+
