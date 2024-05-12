82916125a4564de8c4d22fc065c8d4b7bee2fa4c
Olga Khamitova <olgacherkasova13317@rambler.ru>
2024-05-12 17:54:59 +0300

    Rename new test

--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -18,7 +18,7 @@ public class StatisticsServiceTest {
     }
 
     @Test
-    public void findMaxIfIncome() {
+    public void findMaxIfFirstElementNotHighest() {
         StatisticsService service = new StatisticsService();
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12, 15};
         long expected = 15;
